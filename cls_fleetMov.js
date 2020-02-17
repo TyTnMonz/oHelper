22f9514f712d7e64f1449484eef66973b4a7f20c
TyTnMonz <marco.monza86@gmail.com>
2020-02-17 20:33:02 +0100

    initial version of cls_fleetMov.js

---
+++ cls_fleetMov.js
@@ -0,0 +1,43 @@
+class cslFleetMov {
+        constructor(id) {
+            this._id = id;
+        }
+
+        get start_coords() {
+            return this._start_coords;
+        }
+        set start_coords(x) {
+            this._start_coords = x;
+        }
+
+        get final_coords() {
+            return this._final_coords;
+        }
+        set final_coords(x) {
+            this._final_coords = x;
+        }
+
+        get starting_datetime() {
+            return this._starting_datetime;
+        }
+        set starting_datetime(x) {
+            this._starting_datetime = x;
+        }
+
+        get arrival_datetime() {
+            return this._arrival_datetime;
+        }
+        set arrival_datetime(x) {
+            x = x[0].getElementsByClassName('tooltipHTML');
+            x = x[0].getAttribute('title');
+            x = x.split('| ')[1].replace('<br>',' ');
+            this._arrival_datetime = get_FormattedLocalTime(new Date(x), true);
+        }
+
+        set data_return(x) {
+            this._data_return = x;
+        }
+        get isOnWayBack() {
+            return ( this._data_return == '1' ) ? true:false;
+        }
+    }
