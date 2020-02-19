30e0989b2a3d7d83f79113c9d73493c4b7a5fb44
TyTnMonz <marco.monza86@gmail.com>
2020-02-19 08:56:33 +0100

    debug get_DateFromDOM_Stirng

--- oh_Utils.js
+++ oh_Utils.js
@@ -9,19 +9,21 @@ function get_TitleAttributeFromDOM(oTag) {
 function get_DateFromDOM_Stirng(sDOM) {
     // Gets the Date in IT Format printed inside the DOM ( dd.MM.yyyy hh:mm:ss )
     // and converts it to the Standard ISO Format ( yyyy-MM-dd HH:mm:ss ) to create a Date Object
-
-    // Sanitize the DOM String
-    if ( sDOM.indexOf('<br>') > 0 ) { sDOM = sDOM.replace('<br>', ' '); }
-    if ( sDOM.indexOf('&lt;br&gt;') > 0 ) { sDOM = sDOM.replace('&lt;br&gt;', ' '); }
-    if ( sDOM.indexOf('| ') > 0 ) { sDOM = sDOM.split('| ')[1]; }
-
-    // Split the DOM String in Date & Time
     console.log(' DOM String : ' + sDOM);
-    const dataString = sDOM.split(' ');
-    // Date
-    const dataStringDate = dataString[0].split('.');
-    // Time
-    const dataStringTime = dataString[1].split(':');
-    // Recreating Date from ISO Format
-    return new Date(dataStringDate[2], dataStringDate[1], dataStringDate[0], dataStringTime[0], dataStringTime[1], dataStringTime[2]);
+    if ( sDOM != '' ) {
+      // Sanitize the DOM String
+      if ( sDOM.indexOf('<br>') > 0 ) { sDOM = sDOM.replace('<br>', ' '); }
+      if ( sDOM.indexOf('&lt;br&gt;') > 0 ) { sDOM = sDOM.replace('&lt;br&gt;', ' '); }
+      if ( sDOM.indexOf('| ') > 0 ) { sDOM = sDOM.split('| ')[1]; }
+
+      // Split the DOM String in Date & Time
+      const dataString = sDOM.split(' ');
+      // Date
+      const dataStringDate = dataString[0].split('.');
+      // Time
+      const dataStringTime = dataString[1].split(':');
+      // Recreating Date from ISO Format
+      return new Date(dataStringDate[2], dataStringDate[1], dataStringDate[0], dataStringTime[0], dataStringTime[1], dataStringTime[2]);
+    }
+    return '';
 }
