d5ece1fa2d5e3ca52bc48e7441938a8c5f679fff
paolopv-it <paolo.caggiula@gmail.com>
2020-04-04 21:18:31 +0200

    mofidicate impostazioni!

--- oh_Main.js
+++ oh_Main.js
@@ -164,6 +164,31 @@ div.localClock {
     top: 10px;
 }
 
+#ohOverlay {
+    position: fixed;
+    display: none;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0,0,0,0.8);
+    z-index: 2;
+}
+
+#ohOverlayDialog{
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  font-size: 50px;
+  color: white;
+  transform: translate(-50%,-50%);
+  background-color: rgba(0,0,0,1);
+  width: 360px;
+  height: 200px;
+}
+
 `;
 
         document.head.appendChild(css);
--- oh_Settings.js
+++ oh_Settings.js
@@ -14,8 +14,29 @@ function addToLeftMenuButtons(){
   oAnchor.setAttribute('target', '_self');
   oAnchor.setAttribute('id', 'oHelper');
   oAnchor.appendChild(oSpanL);
+  oAnchor.addEventListener('click', openSettingDialog);
 
   oLi.appendChild(oSpanR);
   oLi.appendChild(oAnchor);
   document.getElementById('menuTableTools').appendChild(oLi);
+
+  //Appending DIV overlay to the bottom of the body
+  let ohOverlayDialog = document.createElement('div');
+  ohOverlayDialog.setAttribute('id', 'ohOverlayDialog');
+  ohOverlayDialog.innerHTML = 'Ciao';
+
+  let ohOverlay = document.createElement('div');
+  ohOverlay.setAttribute('id', 'ohOverlay');
+  ohOverlay.addEventListener('click', closeSettingDialog);
+  ohOverlay.appendChild(ohOverlayDialog);
+
+  document.body.appendChild(ohOverlay);
+}
+
+function openSettingDialog(){
+  document.getElementById("ohOverlay").style.display = "block";
+}
+
+function closeSettingDialog(){
+  document.getElementById("ohOverlay").style.display = "none";
 }
